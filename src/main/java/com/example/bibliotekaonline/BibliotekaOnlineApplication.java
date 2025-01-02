7da6a044d0d650f6315d9b3b3e2124d7697d013c
Wiktor Sieracki <w.sieracki.643@studms.ug.edu.pl>
2025-01-02 13:38:00 +0100

    basic spring

---
+++ src/main/java/com/example/bibliotekaonline/BibliotekaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotekaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotekaOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotekaOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bibliotekaonline/BibliotekaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotekaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotekaOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
